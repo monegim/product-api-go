28b34753fbb0dc2d303a5f1c9f5549e949b6ecbc
Mostafa Negim <mostafa.negim@gmail.com>
2022-12-12 16:13:17 +0330

    feat: add coffee model

---
+++ data/model/coffee.go
@@ -0,0 +1,62 @@
+package model
+
+import (
+	"database/sql"
+	"encoding/json"
+	"io"
+)
+
+// Coffees is a list of Coffee
+type Coffees []Coffee
+
+// FromJSON serializes data from json
+func (c *Coffees) FromJSON(data io.Reader) error {
+	de := json.NewDecoder(data)
+	return de.Decode(c)
+}
+
+// ToJSON converts the collection to json
+func (c *Coffees) ToJSON() ([]byte, error) {
+	return json.Marshal(c)
+}
+
+// Coffee defines a coffee in the database
+type Coffee struct {
+	ID          int                `db:"id" json:"id"`
+	Name        string             `db:"name" json:"name"`
+	Teaser      string             `db:"teaser" json:"teaser"`
+	Collection  string             `db:"collection" json:"collection"`
+	Origin      string             `db:"origin" json:"origin"`
+	Color       string             `db:"color" json:"color"`
+	Description string             `db:"description" json:"description"`
+	Price       float64            `db:"price" json:"price"`
+	Image       string             `db:"image" json:"image"`
+	CreatedAt   string             `db:"created_at" json:"-"`
+	UpdatedAt   string             `db:"updated_at" json:"-"`
+	DeletedAt   sql.NullString     `db:"deleted_at" json:"-"`
+	Ingredients []CoffeeIngredient `json:"ingredients"`
+}
+
+func (c *Coffee) FromJSON(data io.Reader) error {
+	de := json.NewDecoder(data)
+	return de.Decode(c)
+}
+
+// ToJSON converts the collection to json
+func (c *Coffee) ToJSON() ([]byte, error) {
+	return json.Marshal(c)
+}
+
+type CoffeeIngredient struct {
+	ID           int            `db:"id" json:"-"`
+	CoffeeID     int            `db:"coffee_id" json:"-"`
+	IngredientID int            `db:"ingredient_id" json:"ingredient_id"`
+	CreatedAt    string         `db:"created_at" json:"-"`
+	UpdatedAt    string         `db:"updated_at" json:"-"`
+	DeletedAt    sql.NullString `db:"deleted_at" json:"-"`
+}
+
+// ToJSON converts the collection to json
+func (c *CoffeeIngredient) ToJSON() ([]byte, error) {
+	return json.Marshal(c)
+}
