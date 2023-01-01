d1b31a471c90785b760efdee7a3eb7d81bb97e32
Mostafa Negim <mostafa.negim@gmail.com>
2023-01-01 19:38:38 +0330

    add Liveness

--- handlers/health.go
+++ handlers/health.go
@@ -1,8 +1,20 @@
 package handlers
 
-import "github.com/hashicorp/go-hclog"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/hashicorp/go-hclog"
+)
 
 type Health struct {
 	logger hclog.Logger
 }
 
+func NewHealth(l hclog.Logger) *Health {
+	return &Health{l}
+}
+
+func (h *Health) Liveness(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(rw, "%s", "ok")
+}
