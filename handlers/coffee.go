62303825230aa5cf604fbbce64a0f34b588b6c70
Mostafa Negim <mostafa.negim@gmail.com>
2023-01-02 10:39:56 +0330

    add coffee methods

--- handlers/coffee.go
+++ handlers/coffee.go
@@ -1,6 +1,11 @@
 package handlers
 
-import "github.com/hashicorp/go-hclog"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/hashicorp/go-hclog"
+)
 
 type Coffee struct {
 	log hclog.Logger
@@ -8,4 +13,14 @@ type Coffee struct {
 
 func NewCoffee(l hclog.Logger) *Coffee {
 	return &Coffee{l}
+}
+
+func (c *Coffee) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	c.log.Info("Handle Coffee")
+	fmt.Fprintf(rw, "coffee")
+
+}
+
+func (c *Coffee) CreateCoffee(_ int, rw http.ResponseWriter, r *http.Request) {
+
 }
