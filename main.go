3e747c8bc24dcaee57beff90258ead1477a55c45
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-05 22:46:03 +0430

    Update: main.go

--- main.go
+++ main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/monegim/product-api-go/pkg/setting"
+	"github.com/monegim/product-api-go/routers"
 )
 
 func main() {
 	gin.SetMode(setting.ServerSetting.RunMode)
-}
 
-func GetCoffeeHandler(c *gin.Context) {
-
+	routersInit := routers.InitRouter()
+	readTimeout := setting.ServerSetting.ReadTimeout
+	writeTimeout := setting.ServerSetting.WriteTimeout
+	endPoint := fmt.Sprintf(":%d", setting.ServerSetting.HttpPort)
+	maxHeaderBytes := 1 << 20
+	server := &http.Server{
+		Addr:           endPoint,
+		Handler:        routersInit,
+		ReadTimeout:    readTimeout,
+		WriteTimeout:   writeTimeout,
+		MaxHeaderBytes: maxHeaderBytes,
+	}
+	log.Printf("[info] start http server listening %s", endPoint)
+	server.ListenAndServe()
 }
