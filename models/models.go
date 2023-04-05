9fc8fa9c74f7f2fd0ecfc8380b3acdfef5b28350
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-06 01:02:05 +0430

    add model

---
+++ models/models.go
@@ -0,0 +1,16 @@
+package models
+
+import "gorm.io/gorm"
+
+var db *gorm.DB
+
+type Model struct {
+	ID         int `gorm:"primary_key" json:"id"`
+	CreatedOn  int `json:"created_on"`
+	ModifiedOn int `json:"modified_on"`
+	DeletedOn  int `json:"deleted_on"`
+}
+
+func Setup() {
+	var err error
+}
