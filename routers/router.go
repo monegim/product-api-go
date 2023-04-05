863386058abe671354fe44e5217ff4728f7c0e3e
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-06 00:29:09 +0430

    add swagger endpoint

--- routers/router.go
+++ routers/router.go
@@ -1,11 +1,16 @@
 package routers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
 
 func InitRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	return r
 }
