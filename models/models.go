1c79a8197c5b08a79820e35cf324df1b5db9eb09
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-07 23:15:38 +0430

    Update: modeles/models.go

--- models/models.go
+++ models/models.go
@@ -1,6 +1,13 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"log"
+	"time"
+
+	"github.com/monegim/product-api-go/pkg/setting"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 var db *gorm.DB
 
@@ -13,4 +20,36 @@ type Model struct {
 
 func Setup() {
 	var err error
+
+	_, err = gorm.Open(sqlite.Open(setting.DatabaseSetting.Path), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalf("models.Setup err: %v", err)
+	}
+	
+	// db.Config.DryRun()
+	// db.Table().Si
+	// db.Callback().Create().Replace("gorm:update_time_stamp", updateTimeStampForCreateCallback)
+	// db.Callback().Update().Replace("gorm:update_time_stamp", updateTimeStampForUpdateCallback)
+	// db.Callback().Delete().Replace("gorm:delete", deleteCallback)
 }
+
+// func CloseDB()  {
+// 	defer db.
+// }
+
+func updateTimeStampForCreateCallback(scope *gorm.DB) {
+	// if  scope.Error  != nil {
+	// 	nowTime := time.Now().Unix()
+	// 	if createTimeField , ok := scope.Statement.Schema.FieldsByName("CreatedOn"); ok {
+
+	// 	}
+	// if createTimeField, ok := scope.Fiel
+	// }
+}
+func updateTimeStampForUpdateCallback(scope *gorm.DB) {
+	if _, ok := scope.Get("gorm:update_column"); !ok {
+		scope.Statement.SetColumn("ModifiedOn", time.Now().Unix())
+	}
+}
+func deleteCallback(scope *gorm.DB) {}
