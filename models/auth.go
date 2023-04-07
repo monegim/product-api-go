11c5ef870022fc4f67a2c1e4bdec0c1424c76410
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-08 01:25:49 +0430

    Add models/auth.go

--- models/auth.go
+++ models/auth.go
@@ -1,5 +1,21 @@
 package models
 
+import "gorm.io/gorm"
+
+type Auth struct {
+	ID       int    `gorm:"primary_key" json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 func CheckAuth(username, password string) (bool, error) {
-	db.Select()
+	var auth Auth
+	err := db.Select("id").Where(Auth{Username: username, Password: password}).First(&auth).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return false, err
+	}
+	if auth.ID > 0 {
+		return true, nil
+	}
+	return false, nil
 }
