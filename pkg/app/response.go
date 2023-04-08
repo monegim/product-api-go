439095055261cbba382e361b98af5d020321b1e8
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-09 02:19:51 +0430

    update pkg/app/response.go

--- pkg/app/response.go
+++ pkg/app/response.go
@@ -5,3 +5,17 @@ import "github.com/gin-gonic/gin"
 type Gin struct {
 	C *gin.Context
 }
+
+type Response struct {
+	Code int         `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
+}
+
+func (g *Gin) Response(httpCode, errCode int, data interface{}) {
+	g.C.JSON(httpCode, Response{
+		Code: errCode,
+		// Msg: e.,
+		Data: data,
+	})
+}
