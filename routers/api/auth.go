9cdcf993ccd26e506838975b2eb34fa2e00b9656
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-13 02:09:04 +0430

    update: routers/api/auth.go

--- routers/api/auth.go
+++ routers/api/auth.go
@@ -34,5 +34,11 @@ func GetAuth(c *gin.Context) {
 		appG.Response(http.StatusUnauthorized, e.ERROR_AUTH, nil)
 		return
 	}
-	token, err := util.GenerateToken()
+	token, err := util.GenerateToken(username, password)
+	if err != nil {
+		appG.Response(http.StatusInternalServerError, e.ERROR_AUTH_TOKEN, nil)
+	}
+	appG.Response(http.StatusOK, e.SUCCESS, map[string]string{
+		"token": token,
+	})
 }
