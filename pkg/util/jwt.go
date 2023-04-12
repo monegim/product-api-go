5a48fc83918d95afaefa9917f975eb1dec54caff
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-13 02:26:58 +0430

    add GenerateToken func

--- pkg/util/jwt.go
+++ pkg/util/jwt.go
@@ -14,6 +14,19 @@ type Claims struct {
 	jwt.StandardClaims
 }
 
-func GenerateToken(username, password string) (string, error)  {
+func GenerateToken(username, password string) (string, error) {
 	nowTime := time.Now()
+	expireTime := nowTime.Add(3 * time.Hour)
+
+	claims := Claims{
+		EncodeMD5(username),
+		EncodeMD5(password),
+		jwt.StandardClaims{
+			ExpiresAt: expireTime.Unix(),
+			Issuer:    "gin-blog",
+		},
+	}
+	tokenClaims := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	token, err := tokenClaims.SignedString(jwtSecret)
+	return token, err
 }
