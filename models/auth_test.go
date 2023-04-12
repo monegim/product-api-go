e6a7c3d5d1b5be2010a158d44c6c4854a03f0f36
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-12 23:41:59 +0430

    get db location

--- models/auth_test.go
+++ models/auth_test.go
@@ -1,5 +1,29 @@
 package models
 
-func setup() {
+import (
+	"fmt"
+	"os"
+	"path"
+	"testing"
 
+	"github.com/go-ini/ini"
+)
+
+var conf_location = "conf/app.ini"
+
+func setup() {
+	cwd, _ := os.Getwd()
+	conf_full_path := path.Join(path.Dir(cwd),conf_location)
+	file, err := ini.Load(conf_full_path)
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
+	db_name := file.Section("database").Key("Path")
+	db_path := path.Join(path.Join(cwd),"..", db_name.String())
+	fmt.Printf("file: %v\n", db_path)
+}
+
+func TestMain(m *testing.M) {
+	setup()
+	m.Run()
 }
