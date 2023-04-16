8a845515cb0f2ae4ed47594f9c4c61fddbec02e8
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-16 15:59:19 +0430

    update: JWT method

--- middleware/jwt/jwt.go
+++ middleware/jwt/jwt.go
@@ -1,15 +1,43 @@
 package jwt
 
 import (
+	"go/token"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"github.com/monegim/product-api-go/pkg/e"
+	"github.com/monegim/product-api-go/pkg/util"
 )
 
 func JWT() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
+	return func(c *gin.Context) {
 		var code int
 		var data interface{}
 
 		code = e.SUCCESS
+		token := c.Query("token")
+		if token == "" {
+			code = e.INVALID_PARAMS
+		} else {
+			_, err := util.ParseToken(token)
+			if err != nil {
+				switch err.(*jwt.ValidationError).Errors {
+				case jwt.ValidationErrorExpired:
+					code = e.ERROR_AUTH_CHECK_TOKEN_TIMEOUT
+				default:
+					code = e.ERROR_AUTH_CHECK_TOKEN_FAIL
+				}
+			}
+		}
+		if code != e.SUCCESS {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"code": code,
+				"data": data,
+			})
+			c.Abort()
+			return
+		}
+		c.Next()
 	}
 }
