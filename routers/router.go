f864fc656678a08bef1760149b09459db9ee3151
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-16 16:09:37 +0430

    update: router.go

--- routers/router.go
+++ routers/router.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/monegim/product-api-go/middleware/jwt"
 	"github.com/monegim/product-api-go/routers/api"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -16,6 +17,6 @@ func InitRouter() *gin.Engine {
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	apiv1 := r.Group("/api/v1")
-	apiv1.Use(jwt.)
+	apiv1.Use(jwt.JWT())
 	return r
 }
