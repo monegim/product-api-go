ccd1da64b90fbc5b956b249d058e989fc7249c64
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-21 23:49:51 +0430

    add MustInt method

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,7 +1,16 @@
 package util
 
-import "github.com/monegim/product-api-go/pkg/setting"
+import (
+	"strconv"
 
-func Setup()  {
-	jwtSecret =[]byte(setting.AppSetting.JwtSecret)
+	"github.com/monegim/product-api-go/pkg/setting"
+)
+
+func Setup() {
+	jwtSecret = []byte(setting.AppSetting.JwtSecret)
+}
+
+func MustInt(s string) int {
+	v, _ := strconv.ParseInt(s, 10, 0)
+	return int(v)
 }
