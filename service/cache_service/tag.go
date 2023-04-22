4cb82742165e12571c3b75d37c79e466934e26ab
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-22 21:10:46 +0430

    Update: GetTagsKey

--- service/cache_service/tag.go
+++ service/cache_service/tag.go
@@ -1,5 +1,12 @@
 package cache_service
 
+import (
+	"strconv"
+	"strings"
+
+	"github.com/monegim/product-api-go/pkg/e"
+)
+
 type Tag struct {
 	ID    int
 	Name  string
@@ -7,4 +14,24 @@ type Tag struct {
 
 	PageNum  int
 	PageSize int
+}
+
+func (t *Tag) GetTagsKey() string {
+	keys := []string{
+		e.CACHE_TAG,
+		"LIST",
+	}
+	if t.Name != "" {
+		keys = append(keys, t.Name)
+	}
+	if t.State >= 0 {
+		keys = append(keys, strconv.Itoa(t.State))
+	}
+	if t.PageNum > 0 {
+		keys = append(keys, strconv.Itoa(t.PageNum))
+	}
+	if t.PageSize > 0 {
+		keys = append(keys, strconv.Itoa(t.PageSize))
+	}
+	return strings.Join(keys, "_")
 }
