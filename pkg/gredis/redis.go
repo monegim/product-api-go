c96d535d0d51a3d750a3181fbdb32c5310919662
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-23 10:54:59 +0430

    Update: redis setup method

--- pkg/gredis/redis.go
+++ pkg/gredis/redis.go
@@ -1,6 +1,9 @@
 package gredis
 
 import (
+	"context"
+	"net"
+
 	"github.com/monegim/product-api-go/pkg/setting"
 	"github.com/redis/go-redis/v9"
 )
@@ -8,10 +11,18 @@ import (
 var RedisConn *redis.Client
 
 func Setup() error {
-	RedisConn := redis.NewClient(&redis.Options{
+	RedisConn = redis.NewClient(&redis.Options{
 		MaxIdleConns:    setting.RedisSetting.MaxIdle,
 		ConnMaxIdleTime: setting.RedisSetting.IdleTimeout,
 		Addr:            setting.RedisSetting.Host,
+		Dialer: func(ctx context.Context, network, addr string) (net.Conn, error) {
+			c, err :=  net.Dial(network, addr)
+			if err != nil {
+				return nil, err
+			}
+			// TODO: Handle password
+			return c, err	
+		},
 	})
-
+	return nil
 }
