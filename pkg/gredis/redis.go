3894497e4517b431ce846a32b1a18d6ca63383b8
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-23 14:44:10 +0430

    Update: redis.go

--- pkg/gredis/redis.go
+++ pkg/gredis/redis.go
@@ -6,23 +6,37 @@ import (
 
 	"github.com/monegim/product-api-go/pkg/setting"
 	"github.com/redis/go-redis/v9"
+	"github.com/sirupsen/logrus"
 )
 
 var RedisConn *redis.Client
 
 func Setup() error {
+	logrus.Info("Initializing gredis")
 	RedisConn = redis.NewClient(&redis.Options{
 		MaxIdleConns:    setting.RedisSetting.MaxIdle,
 		ConnMaxIdleTime: setting.RedisSetting.IdleTimeout,
 		Addr:            setting.RedisSetting.Host,
 		Dialer: func(ctx context.Context, network, addr string) (net.Conn, error) {
-			c, err :=  net.Dial(network, addr)
+			c, err := net.Dial(network, addr)
 			if err != nil {
 				return nil, err
 			}
 			// TODO: Handle password
-			return c, err	
+			return c, err
 		},
 	})
 	return nil
+}
+
+// Exists check a key
+func Exists(key string) bool {
+	conn := RedisConn.Conn()
+	defer conn.Close()
+	// conn.Exist
+	return true
+}
+
+func Get(key string) ([]byte, error) {
+	return nil, nil
 }
